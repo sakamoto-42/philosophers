9976e9d87f55987c0fc23573c78dff4a444b27c8
Julien Ducharlet <juduchar@student.42perpignan.fr>
2025-03-25 16:47:40 +0100

    clean

---
+++ routine_actions.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_actions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 16:44:55 by juduchar          #+#    #+#             */
+/*   Updated: 2025/03/25 16:47:23 by juduchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_eat(t_philo *philo)
+{
+	ft_lock_forks(philo);
+	ft_print_info(philo, "is eating \n");
+	usleep(philo->data->time_to_eat * 1000);
+	ft_unlock_forks(philo);
+}
+
+void	ft_sleep(t_philo *philo)
+{
+	ft_print_info(philo, "is sleeping\n");
+	usleep(philo->data->time_to_sleep * 1000);
+}
+void	ft_think(t_philo *philo)
+{
+	ft_print_info(philo, "is thinking\n");
+}
---
+++ routine_actions_utils.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_actions_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 16:45:38 by juduchar          #+#    #+#             */
+/*   Updated: 2025/03/25 16:45:56 by juduchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_lock_forks(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		ft_print_info(philo, "has taken a fork\n");
+		pthread_mutex_lock(philo->right_fork);
+		ft_print_info(philo, "has taken a fork\n");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->right_fork);
+		ft_print_info(philo, "has taken a fork\n");
+		pthread_mutex_lock(philo->left_fork);
+		ft_print_info(philo, "has taken a fork\n");
+	}
+}
+
+void	ft_unlock_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
